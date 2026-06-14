948bf3bba29e63050e208655d76f3adfeea7c737
evelman2001-lgtm <evelman2001@gmail.com>
2026-06-14 19:19:07 +0900

    3-09 질문삭제 진행필요

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -45,6 +45,12 @@ public class QuestionService {
             q.setCreatedDate(LocalDateTime.now());
             this.questionRepository.save(q);
         }
+        public void modify(Question question, String subject, String content) {
+            question.setSubject(subject);
+            question.setContent(content);
+            question.setModifyDate(LocalDateTime.now());
+            this.questionRepository.save(question);
+        }
 
     }
 
